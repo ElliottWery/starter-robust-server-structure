2c7cb34ce5833c6c9700aadc3da106bf66c7757d
ElliottWery <92320166+ElliottWery@users.noreply.github.com>
2022-04-22 11:49:59 -0700

    changes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,39 @@
 const express = require("express");
 const app = express();
 
+const counts = require("./data/counts-data");
+const flips = require("./data/flips-data");
+
 // TODO: Follow instructions in the checkpoint to implement ths API.
+app.use("/counts/:countId", (req, res, next) => {
+  const { countId } = req.params;
+  const foundCount = counts[countId];
+
+  if (foundCount === undefined) {
+    next(`Count id not found" ${countId}`);
+  } else {
+    res.json({ data: foundCount });
+  }
+});
+
+app.use("/counts", (req, res) => {
+  res.json({ data: counts });
+});
+
+app.use("/flips/:flipId", (req, res, next) => {
+  const { flipId } = req.params;
+  const foundFlip = flips.find((flip) => flip.id === Number(flipId));
+  if (foundFlip) {
+    res.json({ data: foundFlip });
+  } else {
+    next(`Flip id not found: ${flipId}`);
+  }
+});
+
+app.use("/flips", (req, res) => {
+  res.json({ data: flips });
+});
+
 
 // Not found handler
 app.use((request, response, next) => {

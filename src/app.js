8d9b384eddd664b52d6699bec05373e5445f1c77
ElliottWery <92320166+ElliottWery@users.noreply.github.com>
2022-04-23 13:30:15 -0700

    changes

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ const app = express();
 const counts = require("./data/counts-data");
 const flips = require("./data/flips-data");
 
-// TODO: Follow instructions in the checkpoint to implement ths API.
+
+app.use(express.json())
+
 app.use("/counts/:countId", (req, res, next) => {
   const { countId } = req.params;
   const foundCount = counts[countId];
@@ -30,10 +32,26 @@ app.use("/flips/:flipId", (req, res, next) => {
   }
 });
 
-app.use("/flips", (req, res) => {
++ app.get("/flips", (req, res) => {
   res.json({ data: flips });
 });
 
+let lastFlipId = flips.reduce((maxId, flip) => Math.max(maxId, flip.id), 0);
+
+app.post("/flips", (req, res, next) => {
+  const { data: { result } = {} } = req.body;
+  if (result) {
+    const newFlip = {
+      id: ++lastFlipId, // Increment last id then assign as the current ID
+      result,
+    };
+    flips.push(newFlip);
+    counts[result] = counts[result] + 1; // Increment the counts
+    res.status(201).json({ data: newFlip });
+  } else {
++    res.sendStatus(400);
+  }
+});
 
 // Not found handler
 app.use((request, response, next) => {
